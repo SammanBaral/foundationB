f4e1fec681e345553f2b8fdeb3cb3cd6c7895a54
SammanBaral <bulkedout.2@gmail.com>
2022-09-09 13:32:25 +0545

    Project Started

---
+++ intro.py
@@ -0,0 +1,5 @@
+#name=(input("enter your name"))
+#age=(input("enter your age"))
+name,age=input("please enter your name and age (comma separated)").split(",")
+print(f"hi! your name is {name} you are {age}")
+
